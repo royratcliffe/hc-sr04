38d409d6ca3fc157e8f88bf1f172763ac1066956
Roy Ratcliffe <roy@ratcliffe.me>
2026-06-02 16:16:17 +0100

    refactor: simplify call_at_exit implementation
    Replace custom memory management in call_at_exit with a
    call_at function for better readability and maintainability.
    Update clean_up logic to use call_up for consistency.

--- call_at_exit.c
+++ call_at_exit.c
@@ -1,50 +1,20 @@
 #include "call_at_exit.h"
+#include "call_at.h"
 #include "cons.h"
 
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-/*!
- * \brief Alternative container-of macro without statement expression.
- * \param ptr Pointer to member.
- * \param type Type of container structure.
- * \param member Name of container's member.
- */
-#define containerof(ptr, type, member) ((type *)((char *)(ptr) - offsetof(type, member)))
-
-struct call_at_exit {
-  struct cons cell;
-  void (*func)(void *);
-};
-
 static void set_up(void) __attribute__((constructor));
 static void clean_up(void) __attribute__((destructor));
 
-static struct cons *head = CONS_NIL;
+static struct cons *at_exit = CONS_NIL;
 
-void call_at_exit(void (*func)(void *), void *arg1) {
-  struct call_at_exit *call = malloc(sizeof(struct call_at_exit));
-  if (call == NULL) {
-    perror("Failed to allocate memory for call_at_exit entry");
-    exit(EXIT_FAILURE);
-  }
-  cons_init(&call->cell, arg1);
-  call->func = func;
-  (void)cons(&head, &call->cell);
-}
+void call_at_exit(void (*func)(void *), void *arg1) { call_at(&at_exit, func, arg1); }
 
-void free_at_exit(void *heap) {
-  call_at_exit(free, heap);
-}
+void free_at_exit(void *heap) { call_at_exit(free, heap); }
 
 static void set_up(void) { atexit(clean_up); }
 
-static void clean_up(void) {
-  struct cons *popped;
-  while (CONS_NOT_NIL_P(popped = cons_pop(&head))) {
-    struct call_at_exit *call = containerof(popped, struct call_at_exit, cell);
-    call->func(cons_car(popped));
-    free(call);
-  }
-}
+static void clean_up(void) { call_up(&at_exit); }
