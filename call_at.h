87a25e515972abeed32f90d5a4e253e60368e7f9
Roy Ratcliffe <roy@ratcliffe.me>
2026-06-15 09:33:26 +0100

    refactor: improve documentation for call_at and call_up functions
    Enhance the comments for the call_at and call_up functions
    to provide clearer descriptions of their parameters and purpose.

--- call_at.h
+++ call_at.h
@@ -1,5 +1,15 @@
 struct cons;
 
+/*!
+ * \brief Schedule a function to be called.
+ * \param ca Pointer to a pointer to the cons list of scheduled functions.
+ * \param func Function pointer to the function to be called.
+ * \param arg1 Argument to be passed to the function when called.
+ */
 void call_at(struct cons **ca, void (*func)(void *), void *arg1);
 
+/*!
+ * \brief Call all scheduled functions and clear the list.
+ * \param ca Pointer to a pointer to the cons list of scheduled functions.
+ */
 void call_up(struct cons **ca);
