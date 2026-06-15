82d7d24c5505d3b1fdfeb4ec30fb0e446208658e
Roy Ratcliffe <roy@ratcliffe.me>
2026-06-15 10:10:38 +0100

    feat: add dirent_ext functions for path construction and memory freeing
    Introduce dir_entry_path to construct a path string from a directory
    and a dirent entry. Add free_dir_entries to free an array of dirent
    entries, enhancing memory management capabilities.

---
+++ dirent_ext.h
@@ -0,0 +1,18 @@
+#include <dirent.h>
+#include <sys/types.h>
+
+/*!
+ * \brief Construct a path string from a directory and a dirent entry.
+ * \param dir The directory path.
+ * \param entry The dirent entry.
+ * \param path Pointer to a string to store the constructed path.
+ * \return 0 on success, or -1 on error.
+ */
+int dir_entry_path(const char *dir, const struct dirent *entry, char **path);
+
+/*!
+ * \brief Free an array of dirent entries.
+ * \param entries Array of dirent entries to free.
+ * \param num_entries Number of entries in the array.
+ */
+void free_dir_entries(struct dirent **entries, ssize_t num_entries);
