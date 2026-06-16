a11cb152b2e8a8e70d4f4d27072a29ffc0d70c78
Roy Ratcliffe <roy@ratcliffe.me>
2026-06-16 06:16:43 +0100

    docs: enhance documentation for xadd.c file
    Add detailed comments for functions handling Redis stream events,
    including trig and echo events. Clarify the purpose and usage
    of each function to improve code readability and maintainability.

--- xadd.c
+++ xadd.c
@@ -1,3 +1,18 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: 2026, Roy Ratcliffe, Northumberland, United Kingdom
+ */
+
+/*!
+ * \file xadd.c
+ *
+ * \brief Source file for Redis stream addition functions.
+ *
+ * \details This file contains the implementation of functions that handle
+ * adding entries to a Redis stream when certain events occur, such as changes
+ * on GPIO lines. It uses the hiredis library to interact with Redis and the
+ * when.h macros for modular callback registration.
+ */
 #include "pr.h"
 #include "redis.h"
 #include "when.h"
@@ -19,6 +34,12 @@ CAUSES(opt_m, handle_opt_m, const char *optarg) {
   }
 }
 
+/*!
+ * \brief Handle the trig event.
+ *
+ * \details This function is called when the trig event occurs. It checks if the
+ * Redis host is set and attempts to connect to the Redis server if it is.
+ */
 CAUSES(trig, handle_trig) {
   if (redis_host() == NULL) {
     pr_info("Redis host is required to add to Redis stream\n");
@@ -35,6 +56,15 @@ CAUSES(trig, handle_trig) {
   }
 }
 
+/*!
+ * \brief Handle the echo event.
+ *
+ * \param pulse_width_ns The pulse width in nanoseconds to be added to the
+ * trimmed \c hc-sr04 Redis stream.
+ *
+ * \details This function is called when the echo event occurs. It adds an entry
+ * to the Redis stream with the pulse width in nanoseconds.
+ */
 CAUSES(echo, handle_echo, uint64_t pulse_width_ns) {
   if (redisCommand(context, "XADD %s MAXLEN ~ %u * pulse_width_ns %llu", redis_key(), maxlen, pulse_width_ns) == NULL) {
     pr_err("Failed to add entry to Redis stream\n");
