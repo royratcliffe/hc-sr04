345672fe63bf01f866a38c4dd02d12243e52f5e4
Roy Ratcliffe <roy@ratcliffe.me>
2026-06-17 12:54:32 +0100

    feat: add functions to set GPIO line offset and value
    - Implement gpio_line_add_offset_from_name to retrieve the line
      offset based on the line name from available GPIO chips.
    - Add gpio_line_set_value to set the value of a GPIO line.
    - Update gpio_line.h with function prototypes and documentation.

--- gpio_line.c
+++ gpio_line.c
@@ -1,5 +1,16 @@
 #include "gpio_line.h"
-#include "gpio.h"
+
+int gpio_line_add_offset_from_name(struct gpio_line *line, struct gpiod_chip **chips, size_t num_chips) {
+  for (size_t i = 0; i < num_chips; i++) {
+    int offset = gpiod_chip_get_line_offset_from_name(chips[i], line->name);
+    if (offset >= 0) {
+      line->chip = chips[i];
+      line->offset = (unsigned int)offset;
+      return offset;
+    }
+  }
+  return -1;
+}
 
 struct gpiod_line_settings *gpio_line_settings(struct gpio_line *line) {
   if (!line->line_settings) {
@@ -10,7 +21,27 @@ struct gpiod_line_settings *gpio_line_settings(struct gpio_line *line) {
 
 struct gpiod_line_config *gpio_line_config(struct gpio_line *line) {
   if (!line->line_config) {
-    line->line_config = gpiod_line_config_new();
+    /*
+     * Fail if the line settings are not set, since these are required to create
+     * a line config. If the line settings are missing, return NULL to indicate
+     * that the line config cannot be created. This ensures that the line config
+     * is only created when the necessary line settings are available,
+     * preventing potential issues with incomplete configurations or null
+     * pointer dereferences when trying to use the line config without valid
+     * line settings.
+     */
+    if (!line->line_settings) {
+      return NULL;
+    }
+    struct gpiod_line_config *line_config = gpiod_line_config_new();
+    if (!line_config) {
+      return NULL;
+    }
+    if (gpiod_line_config_add_line_settings(line_config, &line->offset, 1, line->line_settings) < 0) {
+      gpiod_line_config_free(line_config);
+      return NULL;
+    }
+    line->line_config = line_config;
   }
   return line->line_config;
 }
@@ -69,4 +100,12 @@ int gpio_line_set_consumer(struct gpio_line *line, const char *consumer) {
   }
   gpiod_request_config_set_consumer(request_config, consumer);
   return 0;
+}
+
+int gpio_line_set_value(struct gpio_line *line, enum gpiod_line_value value) {
+  struct gpiod_line_request *line_request = gpio_line_request(line);
+  if (!line_request) {
+    return -1;
+  }
+  return gpiod_line_request_set_value(line_request, line->offset, value);
 }
--- gpio_line.h
+++ gpio_line.h
@@ -20,6 +20,15 @@ struct gpio_line {
   unsigned int offset;
 };
 
+/*!
+ * \brief Add the line offset for a GPIO line by its name.
+ * \param line Pointer to a gpio_line structure to be filled with the chip and offset information.
+ * \param chips Array of gpiod_chip pointers representing the available GPIO chips.
+ * \param num_chips The number of GPIO chips in the chips array.
+ * \return The line offset if found, or -1 if the line with the specified name is not found in any of the provided chips.
+ */
+int gpio_line_add_offset_from_name(struct gpio_line *line, struct gpiod_chip **chips, size_t num_chips);
+
 struct gpiod_line_settings *gpio_line_settings(struct gpio_line *line);        /*!< Access or create the line settings for a GPIO line */
 struct gpiod_line_config *gpio_line_config(struct gpio_line *line);            /*!< Access or create the line config for a GPIO line */
 struct gpiod_request_config *gpio_line_request_config(struct gpio_line *line); /*!< Access or create the request config for a GPIO line */
@@ -48,5 +57,13 @@ int gpio_line_set_edge_detection(struct gpio_line *line, enum gpiod_line_edge ed
  * \return 0 on success, -1 on failure.
  */
 int gpio_line_set_consumer(struct gpio_line *line, const char *consumer);
+
+/*!
+ * \brief Set the value of a GPIO line.
+ * \param line Pointer to a gpio_line structure representing the GPIO line to configure.
+ * \param value The value to set for the GPIO line (active or inactive).
+ * \return 0 on success, -1 on failure.
+ */
+int gpio_line_set_value(struct gpio_line *line, enum gpiod_line_value value);
 
 #endif /* GPIO_LINE_H */
