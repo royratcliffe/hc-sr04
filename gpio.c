3f520bcacd5aaf4ab60c7e48ed1b547719539b93
Roy Ratcliffe <roy@ratcliffe.me>
2026-06-17 10:12:09 +0100

    fix: enhance edge event generator with initialisation and fetching
    Add initialisation and event fetching functions for the GPIO edge
    event generator. This includes managing the maximum number of events
    and the current index in the event buffer, improving the handling
    of edge events from GPIO line requests.

--- gpio.c
+++ gpio.c
@@ -1,6 +1,43 @@
 #include "gpio.h"
 #include "pr.h"
 
+void gpio_edge_event_generator_init(struct gpio_edge_event_generator *generator, struct gpiod_line_request *request, struct gpiod_edge_event_buffer *buffer,
+                                    size_t max_events) {
+  generator->request = request;
+  generator->buffer = buffer;
+  generator->max_events = max_events;
+  generator->num_events = 0;
+  generator->index = 0;
+}
+
+int gpio_edge_event_generator_next(struct gpio_edge_event_generator *generator, struct gpiod_edge_event **event) {
+  if (!generator || !event) {
+    return -1;
+  }
+  if (generator->index >= generator->num_events) {
+    if (generator->max_events == 0) {
+      return 0;
+    }
+    const size_t capacity = gpiod_edge_event_buffer_get_capacity(generator->buffer);
+    const size_t max_events = generator->max_events < capacity ? generator->max_events : capacity;
+    if (max_events == 0) {
+      return 0;
+    }
+    generator->num_events = gpiod_line_request_read_edge_events(generator->request, generator->buffer, max_events);
+    if (generator->num_events <= 0) {
+      return (int)generator->num_events;
+    }
+    generator->index = 0;
+    generator->max_events -= (size_t)generator->num_events;
+  }
+  *event = gpiod_edge_event_buffer_get_event(generator->buffer, (size_t)generator->index);
+  if (!*event) {
+    return -1;
+  }
+  generator->index++;
+  return 1;
+}
+
 int gpio_line_request_read_edge_events(struct gpiod_line_request *request, struct gpiod_edge_event_buffer *buffer, size_t max_events,
                                        int (*yield)(struct gpiod_edge_event *event, void *user_data), void *user_data) {
   int rc = 0;
